76039a4a4b5be554ecd07159f82d8c94c760f03c
Laura C Paulino <40530704+lauxpaux@users.noreply.github.com>
2020-07-14 12:14:39 -0400

    added javascript
    up to video 10. Added styles and some javascript.

--- 4th-kind/final-code/4thkind.js
+++ 4th-kind/final-code/4thkind.js
@@ -3,20 +3,41 @@ const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 const darkModeToggle = document.querySelector("a.dark-mode-toggle")
 const darkModeToggleText = darkModeToggle.querySelector("span")
 
+const menuToggle = document.querySelector("a.menu-toggle")
+const menuToggleText = menuToggle.querySelector("span")
+
 const bodyTag = document.querySelector("body")
 
 
+menuToggle.addEventListener("click", function() {
+    bodyTag.classList.toggle("nav-open")
+
+    if(bodyTag.classList.contains("nav-open")) {
+        menuToggleText.innerHTML = "Close"
+    } else {
+        menuToggleText.innerHTML = "Menu"
+    }
+})
+
 darkModeToggle.addEventListener("click", function() {
     bodyTag.classList.toggle("dark-mode")
+
+    if(bodyTag.classList.contains("dark-mode")) {
+        darkModeToggleText.innerHTML = "Light mode" 
+    } else {
+        darkModeToggleText.innerHTML = "Dark mode"
+    }
 })
 
 
 
 const updateDarkMode = function() {
     if (mqDark.matches) {
-    darkModeToggleText.innerHTML = "Light mode"
+       bodyTag.classList.add("dark-mode")
+       darkModeToggleText.innerHTML = "Light mode"
  } else {
-    darkModeToggleText.innerHTML = "Dark mode"
+    bodyTag.classList.remove("dark-mode")
+    
  }
 }
 
